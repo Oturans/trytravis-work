4c7934901ffced2e4b65c40796dd92b8ee1a5b64
Andrey Protasovitskiy <fangil_2002@mail.ru>
2020-04-06 09:03:25 +0300

    Fix error in bake.sh

--- packer/files/bake.sh
+++ packer/files/bake.sh
@@ -3,6 +3,8 @@ set -e
 
 # Install Puma
 apt update
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
 curl -o /etc/systemd/system/puma.service https://raw.githubusercontent.com/Otus-DevOps-2020-02/Oturans_infra/packer-base/packer/files/puma.service
 sudo systemctl enable puma.service
 systemctl start puma.service
