a7c8c08d1a475b54ecd72e12677fcc5b2ec6db78
Andrey Protasovitskiy <fangil_2002@mail.ru>
2020-04-13 09:42:38 +0300

    Ansible-2 init

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -18,21 +18,21 @@ resource "google_compute_instance" "app" {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 
-  connection {
-    type        = "ssh"
-    host        = self.network_interface[0].access_config[0].nat_ip
-    user        = "appuser"
-    agent       = false
-    private_key = file(var.private_key_path)
-  }
+  # connection {
+  #   type        = "ssh"
+  #   host        = self.network_interface[0].access_config[0].nat_ip
+  #   user        = "appuser"
+  #   agent       = false
+  #   private_key = file(var.private_key_path)
+  # }
 
-  provisioner "file" {
-    content     = templatefile("${path.module}/files/puma.service.tpl", { database_url = var.database_url })
-    destination = "/tmp/puma.service"
-  }
-  provisioner "remote-exec" {
-    script = "${path.module}/files/deploy.sh"
-  }
+  # provisioner "file" {
+  #   content     = templatefile("${path.module}/files/puma.service.tpl", { database_url = var.database_url })
+  #   destination = "/tmp/puma.service"
+  # }
+  # provisioner "remote-exec" {
+  #   script = "${path.module}/files/deploy.sh"
+  #}
 }
 resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
